2b9ea8c310f2193c91c524e7636d382e18ab0607
alphaOri <david.everett.smith@gmail.com>
2019-09-16 21:23:36 -0400

    database connection working, findOne working

--- my.js
+++ my.js
@@ -2,25 +2,46 @@
 const mongodb = require('mongodb')
 const brain = require('brainjs')
 
-var url = "mongodb://nodered:noderedpassword@192.168.1.19:27017/water"
+//var mongoHostname = "127.0.0.1"
+//var mongoPort = "27017"
+var mongoHostname = "192.168.1.19"
+var mongoPort = "27017"
+var mongoDbName = "water"
+var mongoDailyCollection = "daily"
+var mongousername = "predictionalgorithm"
+var mongopassword = "predictionpassword"
+
+var url = "mongodb://"+mongousername+":"+mongopassword+"@"+mongoHostname+":"+mongoPort+"/"+mongoDbName
+var dbHandle
 var collectionHandle
-/*mongodb.MongoClient.connect(url, {native_parser:true}, function(err, db) {
+mongodb.MongoClient.connect(url, {useNewUrlParser:true}, function(err, client) {
     if(err){
-        node.error(err)
+        console.error(err)
     } else {
         console.log("Connected to database.")
-        collectionHandle = db.collection("daily")
+        dbHandle = client.db(mongoDbName)
+        collectionHandle = dbHandle.collection(mongoDailyCollection)
+        var start = new Date()
+		var end = new Date(start.getTime())
+		start.setHours(0,0,0,0)
+		end.setHours(24,0,0,0)
+
+		collectionHandle.findOne({created: { $lt:start }}, 
+		    {projection: {average: 1, numavg: 1, gallons: 1, incomplete: 1}, sort: { _id: -1 }, limit: 1 }, function(err, doc) {
+		    	if(err){ console.error(err) } 
+		    	else {
+		    		if(doc){
+		    			console.log(doc)
+		    		}else {
+		    			console.log("no doc")
+		    		}
+		    	}
+		    }
+		)
     }
-})*/
+})
 
-mongodb.MongoClient.connect('mongodb://nodered:noderedpassword@192.168.1.19:27017', function (err, client) {
-  if (err) throw err;
 
-  var db = client.db('water');
-
-  collectionHandle = db.collection('daily')
-  console.log("Connected to database.")
-}) 
 
 var net = new brain.NeuralNetwork();
  
